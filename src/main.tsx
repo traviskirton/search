30ec78c6a91f3f1c2bb797d4217576981540eb49
Travis <travis@createwithflow.com>
2026-01-20 13:36:37 -0800

    Update theme, fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import App from './App.tsx'
 import Components from './Components.tsx'
+import { theme } from '../theme/dist/mantine'
 import '@mantine/core/styles.css'
-import '../mantine-theme.css'
 import './global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
