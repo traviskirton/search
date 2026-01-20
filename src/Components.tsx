caa73a8b6d13591bf3744fb3e5730edbef504874
Travis <travis@createwithflow.com>
2026-01-20 10:23:39 -0800

    Add react router, components page

---
+++ src/Components.tsx
@@ -0,0 +1,13 @@
+import { Container, Title, Stack, Box } from '@mantine/core'
+import { SearchResult } from './components/SearchResult'
+
+function Components() {
+  return (
+    <Container size="sm" py="xl">
+      <Title mb="xl">Components</Title>
+
+    </Container>
+  )
+}
+
+export default Components
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
 import App from './App.tsx'
+import Components from './Components.tsx'
 import '@mantine/core/styles.css'
 import '../mantine-theme.css'
 import './global.css'
@@ -9,7 +11,12 @@ import './global.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/components" element={<Components />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>,
 )
