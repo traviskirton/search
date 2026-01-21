441b1ba2d2b6f48a672d71095e09850c64fdb29c
Travis <travis@createwithflow.com>
2026-01-21 11:26:06 -0800

    Add defaults for ui elements
    Best to have a real ds though

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, createTheme } from '@mantine/core'
 import App from './App.tsx'
 import Components from './Components.tsx'
-import { theme } from '../theme/dist/mantine'
+import { theme as baseTheme } from '../theme/dist/mantine'
 import '@mantine/core/styles.css'
 import './global.css'
+
+const theme = createTheme({
+  ...baseTheme,
+  components: {
+    Button: {
+      defaultProps: {
+        fz: 'md',
+      },
+      styles: () => ({
+        root: {
+          fontWeight: baseTheme.fontWeights?.medium,
+          letterSpacing: baseTheme.letterSpacing?.small,
+        },
+      }),
+    },
+    Text: {
+      defaultProps: {
+        fz: 'md',
+      },
+      styles: () => ({
+        root: {
+          fontWeight: baseTheme.fontWeights?.medium,
+          letterSpacing: baseTheme.letterSpacing?.small,
+        },
+      }),
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
